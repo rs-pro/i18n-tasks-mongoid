f65b39c039658c0c578d3b2a566d2ea3dbcd9e42
glebtv <glebtv@gmail.com>
2014-04-04 16:08:36 +0400

    initial

---
+++ lib/i18n/tasks/mongoid.rb
@@ -0,0 +1,9 @@
+require "i18n/tasks/mongoid/version"
+
+module I18n
+  module Tasks
+    module Mongoid
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/i18n/tasks/mongoid/version.rb
@@ -0,0 +1,7 @@
+module I18n
+  module Tasks
+    module Mongoid
+      VERSION = "0.0.1"
+    end
+  end
+end
